27436137e52d32c9b0809ea0de30db92c44c744a
duymai <rucro12345@gmai.com>
2021-06-22 10:57:11 +0700

    test2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   baseUrl: 'http://localhost:8085/api',
   // serviceUrl: 'http://35.193.198.209/my-service-1/api/v1',
-  serviceUrl: 'http://35.193.198.209:8080/api/v1',
+  serviceUrl: 'http://35.202.55.79:8080/api/v1',
   SOCKET_ENDPOINT: 'http://localhost:8000'
 };
